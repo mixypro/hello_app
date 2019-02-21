5ac701b055208c2e83543b0ce281e58e4de9f84c
mixypro <mixypro@yahoo.com>
2019-02-21 22:49:06 +0100

    Done app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+  
+    def hello
+        render html: "¡¡¡Hola, mundo¡¡!"
+      end
+
+      def goodbye
+        render html: "Do svidanjaa"
+      end
+
+    end
